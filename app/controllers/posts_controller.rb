47d6c16bd7c200f5f19066632049b19f726f56af
Jea Seong Yoon <10jsy@naver.com>
2018-12-09 23:59:43 +0000

    create post functionality and callback from post model to page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,28 @@
 class PostsController < ApplicationController
 	#index controller action 
 	def index
+		# get all posts from database post model to index controller action
+		@posts = Post.all
 	end
+
+	#new controller action; create new post with image
+	def new
+		@post = Post.new
+	end
+
+	#create controller action with private method post_params
+	def create
+		@post = Post.create(post_params)
+		#redirects user back to home/posts after creating post
+		redirect_to posts_path
+	end
+
+	# private method ie only available within this class
+	private
+
+		def post_params
+			# only image and description accepted
+			params.require(:post).permit(:image, :description)
+		end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
 	# resize image uploaded to 640x640, like IG
 	has_attached_file :image, styles: { medium: "640x640>" }
 	# validates that the uploaded file is image file
-	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
