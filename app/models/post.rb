0f46c7aa2280a387abdc409f9d4e778ebdf2df48
Jea Seong Yoon <10jsy@naver.com>
2018-12-09 23:39:06 +0000

    paperclip image upload validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image
+
+	# resize image uploaded to 640x640, like IG
+	has_attached_file :image, styles: { medium: "640x640>" }
+	# validates that the uploaded file is image file
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_09_225430) do
+ActiveRecord::Schema.define(version: 2018_12_09_232519) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
